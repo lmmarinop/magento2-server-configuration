84e5f6e40bbb5465fba76c7e3c977b095ea7c5b7
lmmarinop <lmmarinop@gmail.com>
2020-04-05 16:46:01 -0500

    Update elasticsearch.sh
    install jdk and elasticsearch

--- install/elasticsearch.sh
+++ install/elasticsearch.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
-apt-get install \
+    
+   wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+   apt-get install apt-transport-https
+   echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
+
+
+   apt-get install default-jdk -y
+   update-alternatives --config java
+   set JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"
+   sed -i '$aJAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"' /etc/profile
+
+    apt-get install \
     elasticsearch \
     --yes
