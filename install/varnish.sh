69401032199bb1de2159620d7b363d469d02fd15
lmmarinop <lmmarinop@gmail.com>
2020-04-06 01:13:11 -0500

    Update varnish.sh
    update installer to v6

--- install/varnish.sh
+++ install/varnish.sh
@@ -1,6 +1,9 @@
 
 #!/usr/bin/env bash
 
+curl https://TOKEN:@packagecloud.io/varnishplus/60/gpgkey | apt-key add -
+echo "deb https://packagecloud.io/varnishcache/varnish60lts/ubuntu/ bionic main" | sudo tee -a /etc/apt/sources.list.d/varnishcache_varnish60lts.list
+
 apt-get install \
     varnish \
     --yes
