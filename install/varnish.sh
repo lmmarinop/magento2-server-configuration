1470b8199c4cdd3883db4d6beed46b4a52801655
lmmarinop <lmmarinop@gmail.com>
2020-04-09 18:28:15 -0500

    Update varnish.sh
    Optional for varnish 6

--- install/varnish.sh
+++ install/varnish.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
-#curl -s https://packagecloud.io/install/repositories/varnishcache/varnish64/script.deb.sh | sudo bash
+if [ $1 == "6" ];
+then
+
 curl -s https://packagecloud.io/install/repositories/varnishcache/varnish60lts/script.deb.sh | sudo bash
 
 apt-get install \
-    #varnish=6.4.0-1~bionic \
     varnish-dev=6.0.6-1~bionic \
     --yes
 
 service varnish restart
+else
+    apt-get install \
+        varnish
+        --yes
+
+    service varnish restart
+
+fi;
