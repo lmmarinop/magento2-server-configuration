b662250664bf85faa8ec2ef2fffb1b3473f2658d
lmmarinop <lmmarinop@gmail.com>
2020-04-09 12:16:58 -0500

    Create varnish_ssl.sh
    configure web server to support ssl termination with varnish

---
+++ install/varnish_ssl.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [ $1 ];
+  then
+    cd /var/www/html/$1/webroot
+    
+    php bin/magento varnish:vcl:generate --access-list localhost --backend-host localhost --backend-port 8080 --export-version 6 --output-file var/default.vcl
+   
+    mv /etc/varnish/default.vcl /etc/varnish/default.vcl.bckp
+    cp /var/www/html/$1/webroot/var/default.vcl  /etc/varnish/default.vcl
+    
+    mv /etc/nginx/sites-available/$1 /etc/nginx/sites-available/no-ssl-current
+    rm -rf /etc/nginx/sites-enabled/$1
+    SITE=/etc/nginx/sites-available/$1
+    curl https://raw.githubusercontent.com/DavidBelicza/magento2-server-configuration/master/config/nginx-site-varnish-ssl > $SITE
+
+    sed -i -e "s/mywebshop.com/$1/g" $SITE
+    ln -s /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled/
+
+    service nginx reload
+  
+  else
+    echo "";
+    echo "1st parameter is magento domain";
+    echo "Try this: varnish_ssl.sh mywebshop.com";
+    echo "";
+fi;
