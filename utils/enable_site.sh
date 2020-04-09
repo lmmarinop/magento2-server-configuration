ba37fe5ac2e7816aa6f89e1a325deb176dd82f58
lmmarinop <lmmarinop@gmail.com>
2020-04-09 14:49:46 -0500

    Update enable_site.sh

--- utils/enable_site.sh
+++ utils/enable_site.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-if[$1];
-then
+if [ $1 ];
+  then
   ln -s /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled/$1
+  else
+    echo "";
+    echo "1st parameter is site domain";
 fi;
