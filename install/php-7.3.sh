03457542189067ad8c53598fee9fe73aab5e1a68
lmmarinop <lmmarinop@gmail.com>
2020-04-10 11:08:37 -0500

    Update php-7.3.sh

--- install/php-7.3.sh
+++ install/php-7.3.sh
@@ -4,22 +4,6 @@ echo -ne '\n' |  add-apt-repository ppa:ondrej/php
 
 apt-get update
 
-apt-get install \
-    php7.3 \ 
-    php7.3-pdo \
-    php7.3-mysqlnd \ 
-    php7.3-opcache \
-    php7.3-xml \
-    php7.3-gd \
-    php7.3-devel 
-    php7.3-mysql \
-    php7.3-intl \
-    php7.3-mbstring \
-    php7.3-bcmath \
-    php7.3-json \
-    php7.3-iconv \
-    php7.3-soap \
-    
-   --yes
+apt-get install php7.3 php7.3-fpm php7.3-opcache php7.3-xml php7.3-gd php7.3-mysql php7.3-intl php7.3-mbstring php7.3-bcmath php7.3-json  php7.3-common  php7.3-soap  --yes
 
 service php7.3-fpm restart
