d984dc00fdeb0949491e696ecca3105c1a958657
lmmarinop <lmmarinop@gmail.com>
2020-04-10 18:36:09 -0500

    Update magento-install.sh

--- install/magento-install.sh
+++ install/magento-install.sh
@@ -8,7 +8,7 @@ if [ $1 ] && [ $2 ] && [ $3 ] && [ $4 ] && [ $5 ] && [ $6 ] && [ $7 ] && [ $8 ] 
         --db-host=$3 --db-name=$4 --db-user=$5 --db-password=$6 \
         --admin-firstname=Developer --admin-lastname=Developer --admin-email=$7 \
         --admin-user=$8 --admin-password=$9 --language=${10} \
-        --currency=${11} --timezone=America/Bogota --use-rewrites=1 --use-secure=1 --use-secure-admin=1 --backend-frontname=${12} \
+        --currency=${11} --timezone=America/Bogota --use-rewrites=1 --use-secure=1 --use-secure-admin=1 --backend-frontname=$12 \
 
         php bin/magento deploy:mode:set developer
         php bin/magento cache:disable
@@ -20,11 +20,11 @@ if [ $1 ] && [ $2 ] && [ $3 ] && [ $4 ] && [ $5 ] && [ $6 ] && [ $7 ] && [ $8 ] 
         chmod u+x bin/magento
         chown -R :www-data .
 
-        service php7.2-fpm restart
+        service php7.3-fpm restart
     else
         echo "";
-        echo "1st parameter is magento domain";
-        echo "2nd parameter is magento domain with http or https";
+        echo "1st parameter is magento domain: ";
+        echo "2nd parameter is magento domain with http or https: ";
         echo "3rd parameter is database host";
         echo "4th parameter is database name";
         echo "5th parameter is database user";
