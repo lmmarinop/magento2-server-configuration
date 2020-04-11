42a754af45d33bd44984fbaad92c784bc4e550da
lmmarinop <lmmarinop@gmail.com>
2020-04-11 17:59:39 -0500

    Update basics.sh

--- install/basics.sh
+++ install/basics.sh
@@ -3,6 +3,9 @@
 sed -i "s/.*#*ClientAliveInterval.*/ClientAliveInterval 1m/" /etc/ssh/sshd_config
 sed -i "s/.*#*ClientAliveCountMax.*/ClientAliveCountMax 3000/" /etc/ssh/sshd_config
 
+systemctl reload sshd
+
+
 
 apt-get install \
     git \
