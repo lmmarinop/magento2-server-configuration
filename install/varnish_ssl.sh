4056d312985b96aed737b0989c10283f9009f1e0
lmmarinop <lmmarinop@gmail.com>
2020-04-10 21:04:17 -0500

    Update varnish_ssl.sh

--- install/varnish_ssl.sh
+++ install/varnish_ssl.sh
@@ -11,8 +11,8 @@ if [ $1 ] && [ $2 ];
     mv /etc/varnish/default.vcl /etc/varnish/default.vcl.bckp
     cp /var/www/html/$1/webroot/var/default.vcl  /etc/varnish/default.vcl
     
-    rm -rf /etc/nginx/sites-enabled/$1
-    mv /etc/nginx/sites-available/$1 /etc/nginx/sites-available/no-ssl-current
+    rm -rf /etc/nginx/sites-available/$1
+  
     
     SITE=/etc/nginx/sites-available/$1
     curl https://raw.githubusercontent.com/lmmarinop/magento2-server-configuration/alternative-stack/config/nginx-site-varnish-ssl > $SITE
