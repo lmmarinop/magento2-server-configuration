cd9766c9958dd72d7d46f73e8565ce62604ea397
lmmarinop <lmmarinop@gmail.com>
2020-04-11 23:46:54 -0500

    Update varnish_ssl.sh

--- install/varnish_ssl.sh
+++ install/varnish_ssl.sh
@@ -6,22 +6,21 @@ if [ $1 ] && [ $2 ];
     
     php bin/magento config:set system/full_page_cache/caching_application 2
     php bin/magento varnish:vcl:generate --access-list localhost --backend-host localhost --backend-port 8080 --export-version $2 --output-file var/default.vcl
-    systemctl stop varnish
-   
     mv /etc/varnish/default.vcl /etc/varnish/default.vcl.bckp
     cp /var/www/html/$1/webroot/var/default.vcl  /etc/varnish/default.vcl
     
-    rm -rf /etc/nginx/sites-enabled/$1
     rm -rf /etc/nginx/sites-available/$1
-        
     SITE=/etc/nginx/sites-available/$1
     curl https://raw.githubusercontent.com/lmmarinop/magento2-server-configuration/alternative-stack/config/nginx-site-varnish-ssl > $SITE
 
+    sed -i "s|/pub/health_check.php|/health_check.php|" /etc/varnish/default.vcl
     sed -i "s/mywebshop.com/$1/" $SITE
+    
     ln -s /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled/
 
-    systemctl start varnish
-    systemctl reload nginx 
+    systemctl restart varnish
+    systemctl restart nginx 
+    systemctl reload  nginx 
   
   else
     echo "";
