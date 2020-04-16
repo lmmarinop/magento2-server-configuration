93d15c547190146d46a5182e8893603854a2240e
lmmarinop <lmmarinop@gmail.com>
2020-04-16 02:25:49 -0500

    Update redis.sh

--- install/redis.sh
+++ install/redis.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
 apt-get install \
-    build-essential \
-    tcl \
+    redis-server \
     --yes
-
-curl -O http://download.redis.io/redis-stable.tar.gz
-tar xzf redis-stable.tar.gz
-cd redis-stable
-make
-#make test
-make install
