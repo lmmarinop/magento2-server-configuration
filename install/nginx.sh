a164b845079ab02b5cd0d41cee8ce2c54d95e3e2
lmmarinop <lmmarinop@gmail.com>
2020-04-17 01:03:48 -0500

    Update nginx.sh

--- install/nginx.sh
+++ install/nginx.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-echo "deb http://nginx.org/packages/mainline/ubuntu `lsb_release -cs` nginx" \
-    | sudo tee /etc/apt/sources.list.d/nginx.list
-curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add -
-
 apt update
 apt-get install \
     nginx \
