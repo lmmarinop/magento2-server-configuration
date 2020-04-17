701a9f0108eb8a2b6feb13161e27fe3698843cd1
lmmarinop <lmmarinop@gmail.com>
2020-04-17 01:03:05 -0500

    Update basics.sh

--- install/basics.sh
+++ install/basics.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sed -i "s/.*#*ClientAliveInterval.*/ClientAliveInterval 1m/" /etc/ssh/sshd_config
-sed -i "s/.*#*ClientAliveCountMax.*/ClientAliveCountMax 3000/" /etc/ssh/sshd_config
+sed -i "s/.*ClientAliveInterval.*/ClientAliveInterval 60/" /etc/ssh/sshd_config
+sed -i "s/.*ClientAliveCountMax.*/ClientAliveCountMax 3000/" /etc/ssh/sshd_config
 
 systemctl reload sshd
 
