130deb1c7275998f3f1df9f90202958a423cd76c
lmmarinop <lmmarinop@gmail.com>
2020-04-17 16:11:08 -0500

    Update magento_post-install.sh

--- install/magento_post-install.sh
+++ install/magento_post-install.sh
@@ -4,7 +4,7 @@ if [ $1 ];
   then
    cd /var/www/html/$1/webroot
   bin/magento config:set catalog/search/engine 'elasticsearch6'
-  echo "yes":"yes" | bin/magento setup:config:set --page-cache=redis --page-cache-redis-server=127.0.0.1 --page-cache-redis-port=6379 --page-cache-redis-db=1
+  #echo "yes":"yes" | bin/magento setup:config:set --page-cache=redis --page-cache-redis-server=127.0.0.1 --page-cache-redis-port=6379 --page-cache-redis-db=1
    
 else
     echo "";
