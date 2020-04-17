db107d5bafdb452e243e0e15a355a949c0bcb7a1
lmmarinop <lmmarinop@gmail.com>
2020-04-17 02:19:39 -0500

    Update varnish.sh

--- install/varnish.sh
+++ install/varnish.sh
@@ -3,8 +3,8 @@
 
 curl -s https://packagecloud.io/install/repositories/varnishcache/varnish64/script.deb.sh | sudo bash
 
-#sudo apt-get install varnish=6.4.0-1~bionic --yes
-sudo apt-get install varnish=6.4.0-1~buster --yes
+sudo apt-get install varnish=6.4.0-1~bionic --yes
+#sudo apt-get install varnish=6.4.0-1~buster --yes
 
 service varnish restart
 
